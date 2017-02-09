361a844e55b97802e3d970d576e4f15c97cba73f
djibouti33 <djibouti33@users.noreply.github.com>
2017-02-08 16:39:05 -0800

    whitespace cleanup

--- Bridges/AppDelegate.swift
+++ Bridges/AppDelegate.swift
@@ -11,33 +11,32 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-    var bridges: [Bridge]?
+  var window: UIWindow?
+  var bridges: [Bridge]?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
-        // read from our bridges.plist and load bridges to be used throughout application 
-        if let path = Bundle.main.path(forResource: "bridge_data/Bridges", ofType: "plist"),
-           let bridgeData = NSArray(contentsOfFile:path) as? [Dictionary<String, AnyObject>] {
-            
-            var bridges = [Bridge]()
-            
-            for dict: Dictionary in bridgeData {
-                let bridge = Bridge(withDictionary: dict)
-                bridges.append(bridge)
-            }
-            
-            self.bridges = bridges;
-        } else {
-            self.bridges = [];
-        }
-        
-        let dict = [String:Any]()
-        if (UserDefaults.standard.dictionary(forKey: "bridge_favorites") == nil) {
-            UserDefaults.standard.set(dict, forKey: "bridge_favorites")
-            UserDefaults.standard.synchronize()
-        }
-        
-        return true
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+  // read from our bridges.plist and load bridges to be used throughout application
+  if let path = Bundle.main.path(forResource: "bridge_data/Bridges", ofType: "plist"),
+    let bridgeData = NSArray(contentsOfFile:path) as? [Dictionary<String, AnyObject>] {
+
+    var bridges = [Bridge]()
+
+    for dict: Dictionary in bridgeData {
+      let bridge = Bridge(withDictionary: dict)
+      bridges.append(bridge)
     }
+    self.bridges = bridges;
+  } else {
+    self.bridges = [];
+  }
+
+  let dict = [String:Any]()
+  if (UserDefaults.standard.dictionary(forKey: "bridge_favorites") == nil) {
+    UserDefaults.standard.set(dict, forKey: "bridge_favorites")
+    UserDefaults.standard.synchronize()
+  }
+
+  return true
+  }
 }
